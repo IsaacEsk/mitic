6498cd9d77d4a449f1db47126331c191c3870602
IsaacEsk <isaac.pineda@eskayser.com.mx>
2026-03-03 15:14:08 -0600

    Primer commit MITIC

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       //home: const SelectCivScreen(), // <--- aquí va tu screen
-      //home: const TableroScreen(), // <--- Solo para probar el diseño
+      home: const TableroScreen(), // <--- Solo para probar el diseño
     );
   }
 }

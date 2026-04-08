94c513e08c8b35c155ce487e0d2280a96e4de3dc
IsaacEsk <isaac.pineda@eskayser.com.mx>
2026-04-08 16:47:14 -0600

    Deploy MITIC 2.0 - Terremotos

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -130,14 +130,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "db32905ed418fb38886a8d041a105b9c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "f5028db1af410f741b9f4d08875fd58d",
+"flutter_bootstrap.js": "d2bb7361383b289248d26f345a0705ab",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "215fb205ca1d23b245d1d4b2f80faf6a",
 "/": "215fb205ca1d23b245d1d4b2f80faf6a",
-"main.dart.js": "291be03efc2d8d2efd2173589106210f",
+"main.dart.js": "bfc4fa30cb5987b3715b37d6711c14fe",
 "manifest.json": "b54293573e0e6c7c275e3d10aa8a42ff",
 "version.json": "301034ed818c925eef89cad8e9d1c175"};
 // The application shell files that are downloaded before a service worker can

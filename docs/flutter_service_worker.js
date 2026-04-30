20e933ef1cbc28b9ff2f94f4b2992306753c0a7b
IsaacEsk <isaac.pineda@eskayser.com.mx>
2026-04-30 17:03:44 -0600

    Deploy MITIC 2.0 - Animaciones mejoradas: resplandores, ataques en cascada, torres selectivas

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -130,14 +130,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "db32905ed418fb38886a8d041a105b9c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "197f2c9e4a393d7be938d508dc124bd6",
+"flutter_bootstrap.js": "7dd9c418261847cd71c55bc1d317ad52",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "215fb205ca1d23b245d1d4b2f80faf6a",
 "/": "215fb205ca1d23b245d1d4b2f80faf6a",
-"main.dart.js": "5defe5f6fb4663de3653504898c9bb8b",
+"main.dart.js": "ee2c76fe446140f77f98a85975f3aac7",
 "manifest.json": "b54293573e0e6c7c275e3d10aa8a42ff",
 "version.json": "301034ed818c925eef89cad8e9d1c175"};
 // The application shell files that are downloaded before a service worker can
